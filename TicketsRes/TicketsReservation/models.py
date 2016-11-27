ab7bd18f96e624f29d9d6cd11e1d5ae8641d7ada
AzVen PM <patryk.matwijczuk@gmail.com>
2016-11-27 11:19:57 +0100

    models

--- TicketsRes/TicketsReservation/models.py
+++ TicketsRes/TicketsReservation/models.py
@@ -14,7 +14,44 @@ class Role(models.Model):
 
 class User(models.Model):
     name = models.CharField(max_length=32)
-    role = models.OneToOneField(Role)
+    surname = models.CharField(max_length=32)
+    email = models.CharField(max_length=32)
+    phone_number = models.CharField(max_length=32)
+    role = models.ForeignKey(Role)
+
+    def __str__(self):
+        return self.name
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=32)
+    address = models.CharField(max_length=32)
+    date = models.DateField()
+    description = models.CharField(max_length=200)
+    website = models.CharField(max_length=32)
+
+    def __str__(self):
+        return self.name
+
+
+class Sector(models.Model):
+    name = models.CharField(max_length=32)
+    max_column = models.IntegerField()
+    max_row = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Tickets(models.Model):
+    event = models.ForeignKey(Event)
+    sector = models.ForeignKey(Sector)
+    column = models.IntegerField()
+    row = models.IntegerField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    guest_name = models.CharField(max_length=32)
+    guest_surname = models.CharField(max_length=32)
+    guest_email = models.CharField(max_length=32)
 
     def __str__(self):
         return self.name
