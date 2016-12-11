0c662d3f1f850584de85b2f80820f7fa63869d06
2korzeniak <Michal.Korzeniak@fis.agh.edu.pl>
2016-12-11 02:10:14 +0100

    Settings corrected (Heroku)

--- TicketsRes/TicketsRes/settings.py
+++ TicketsRes/TicketsRes/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@86a0rq*7k2xvk-4qcpo+@*^22w4%y&&73)w53+)63us9&ml7z'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,4 +121,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
+#STATIC_URL = '/static/'
+# Static asset configuration
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = 'staticfiles'
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
