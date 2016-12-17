bbe98f16af4c72e25a35553b815a9825e22d9e69
Kamill0 <kamillo1493@gmail.com>
2016-12-17 13:37:01 +0100

    Fixed URLs

--- TicketsRes/TicketsReservation/urls.py
+++ TicketsRes/TicketsReservation/urls.py
@@ -18,6 +18,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'', views.index, name='index'),
+    url(r'^$', views.index, name='index'),
     url(r'user/(?P<id>[0-9]+)', views.user, name='user'),
 ]
