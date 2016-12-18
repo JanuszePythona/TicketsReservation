e2bbb3c2a5b0a0c27f42102ed77120fa7cd6b4ce
nabuchodonozor <b.piwnik@o2.pl>
2016-12-18 16:39:44 +0100

    add registration functionality

---
+++ TicketsRes/Login/forms.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import User
+from django.forms import ModelForm
+
+
+# class UserProfileForm(ModelForm):
+#     class Meta:
+#         model = UserProfile
+#         exclude = ['user']
--- TicketsRes/Login/urls.py
+++ TicketsRes/Login/urls.py
@@ -5,6 +5,7 @@ from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
+    url(r'^register/$', views.register, name='register'),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'template_name': 'registration/logged_out.html'}, name='logout'),
     url(r'^user_home/$', views.user_home, name='user_home'),
