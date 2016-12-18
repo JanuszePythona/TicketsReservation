a35f9e956c1a15a5cfd263cbfbc0d0d7f1fe1881
Kamill0 <kamillo1493@gmail.com>
2016-12-18 14:46:27 +0100

    Style added

--- TicketsRes/TicketsReservation/urls.py
+++ TicketsRes/TicketsReservation/urls.py
@@ -19,5 +19,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^about/$', views.about, name='about'),
     url(r'user/(?P<id>[0-9]+)', views.user, name='user'),
 ]
--- TicketsRes/TicketsReservation/views.py
+++ TicketsRes/TicketsReservation/views.py
@@ -18,3 +18,11 @@ def user(request):
 
     }
     return HttpResponse(template.render(context, request))
+
+
+def about(request):
+    template = loader.get_template('about.html')
+    context = {
+
+    }
+    return HttpResponse(template.render(context, request))
