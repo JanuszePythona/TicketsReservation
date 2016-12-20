a4ffecced0e6bb2f52996c53dd8b2b1dfe6741c3
nabuchodonozor <b.piwnik@o2.pl>
2016-12-20 16:18:24 +0100

    fix tests for new models

--- TicketsRes/EventCreator/tests.py
+++ TicketsRes/EventCreator/tests.py
@@ -1,17 +1,19 @@
+from django.contrib.auth.models import User
+
+from models import Event, Sector
 from django.test import TestCase
 
-from EventCreator.models import Event,Sector
-from Login.models import User,Role
 
-# Create your tests here.
 class EventCreatorTestCase(TestCase):
     def setUp(self):
-        self.role1 = Role.objects.create(name='User')
-        self.user1 = User.objects.create(name='Jurand', surname='zeSpychowa', email='jurand@o.pl', phone_number='12343', role=self.role1)
-        self.user2 = User.objects.create(name='Zbyszko', surname='zBogdanca', email='zbyszko@o.pl', phone_number='1233', role=self.role1)
-        self.event1 = Event.objects.create(name='KinoBambino', address='test_adress', description='test_descr',website='web.web',user=self.user1)
-        self.event2 = Event.objects.create(name='Januszowo', address='test_adress1', description='testdscr', website='web.com',user=self.user1)
-        self.event3 = Event.objects.create(name='Andrzejowo', address='test_adress12', description='testdscr1', website='web.com.pl', user=self.user2)
+        self.user1 = User.objects.create(username='admin', first_name='Jurand', last_name='zeSpychowa', email='jurand@o.pl')
+        self.user2 = User.objects.create(username='mie1992', first_name='Zbyszko', last_name='zBogdanca', email='zbyszko@o.pl')
+        self.event1 = Event.objects.create(name='KinoBambino', address='test_adress', description='test_descr',
+                                           website='web.web', user=self.user1)
+        self.event2 = Event.objects.create(name='Januszowo', address='test_adress1', description='testdscr',
+                                           website='web.com', user=self.user1)
+        self.event3 = Event.objects.create(name='Andrzejowo', address='test_adress12', description='testdscr1',
+                                           website='web.com.pl', user=self.user2)
         self.sector1 = Sector.objects.create(name='Sektor1', max_column=5, max_row=10)
 
     def test_eventdata(self):
--- TicketsRes/TicketsReservation/tests.py
+++ TicketsRes/TicketsReservation/tests.py
@@ -1,17 +1,21 @@
+from django.contrib.auth.models import User
+
+from models import Sector, Event
+from models import Tickets
 from django.test import TestCase
-from TicketsReservation.models import Tickets
-from EventCreator.models import Sector, Event
-from Login.models import User,Role
+
 
 # Create your tests here.
 class TicketsReservationTestCase(TestCase):
     def setUp(self):
-        self.role1 = Role.objects.create(name='User')
-        self.user1 = User.objects.create(name='Jurand', surname='zeSpychowa', email='jurand@o.pl',phone_number='12343', role=self.role1)
-        self.event1 = Event.objects.create(name='KinoBambino', address='test_adress', description='test_descr', website='web.web', user=self.user1)
+        self.user1 = User.objects.create(username='admin', first_name='Jurand', last_name='zeSpychowa', email='jurand@o.pl')
+        self.event1 = Event.objects.create(name='KinoBambino', address='test_adress', description='test_descr',
+                                           website='web.web', user=self.user1)
         self.sector1 = Sector.objects.create(name='Sektor1', max_column=5, max_row=10)
-        self.ticket1 = Tickets.objects.create(event=self.event1, sector=self.sector1, column=2, row=3, price=12.01, guest_name='Andrew', guest_surname='Golara', guest_email = 'golara@o.pl')
-        self.ticket2 = Tickets.objects.create(event=self.event1, sector=self.sector1, column=7, row=8, price=15.50, guest_name='Andrzej', guest_surname='Golota', guest_email='golota@o.pl')
+        self.ticket1 = Tickets.objects.create(event=self.event1, sector=self.sector1, column=2, row=3, price=12.01,
+                                              guest_name='Andrew', guest_surname='Golara', guest_email='golara@o.pl')
+        self.ticket2 = Tickets.objects.create(event=self.event1, sector=self.sector1, column=7, row=8, price=15.50,
+                                              guest_name='Andrzej', guest_surname='Golota', guest_email='golota@o.pl')
 
     def test_ticketdata(self):
         self.assertEquals(self.ticket1.event, self.event1)
