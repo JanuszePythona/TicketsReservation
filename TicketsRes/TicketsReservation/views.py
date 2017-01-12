029ea7634ad4e5e0611d43780c00fdc1a3b2debb
Kamill0 <kamillo1493@gmail.com>
2017-01-12 17:28:24 +0100

    reverted changes

--- TicketsRes/TicketsReservation/views.py
+++ TicketsRes/TicketsReservation/views.py
@@ -10,11 +10,12 @@ from forms import TicketForm
 from models import Event
 from models import Sector
 
+
 def index(request):
     template = loader.get_template('index.html')
     context = {
         'users': User.objects.all(),
-        'activities': Event.objects.all()
+        'events': Event.objects.all()
     }
     return HttpResponse(template.render(context, request))
 
@@ -22,8 +23,8 @@ def index(request):
 def view_event(request, event_id):
     template = loader.get_template('event.html')
     context = {
-        'activity': Event.objects.get(id=event_id),
-        'sectors': Sector.objects.filter(event = event_id)
+        'event': Event.objects.get(id=event_id),
+        'sectors': Sector.objects.filter(event=event_id)
     }
     return HttpResponse(template.render(context, request))
 
@@ -40,7 +41,7 @@ def place_reservation(request, event_id):
             mail = EmailMessage('Event entry ticket', ticket.get_reservation_info(), 'janusze.pythona@gmail.com', [ticket.guest_email])
             mail.attach_file(ticket.qrcode.url)
             mail.send()
-            #send_mail('Test', ticket.qrcode, 'janusze.pythona@gmail.com', [email])
+
             return render(request, 'user_home.html')
     else:
         form = TicketForm(event_id)
@@ -62,10 +63,11 @@ def contact(request):
     }
     return HttpResponse(template.render(context, request))
 
+
 @login_required
 def user_home(request):
     template = loader.get_template('user_home.html')
     context = {
-        'activities': Event.objects.filter(user_id=request.user.id)
+        'events': Event.objects.filter(user_id=request.user.id)
     }
     return HttpResponse(template.render(context, request))
