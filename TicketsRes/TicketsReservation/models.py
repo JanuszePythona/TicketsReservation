af9ff2a5647c41bb563d4ec15fd8b8a5f645bfe1
Kamill0 <kamillo1493@gmail.com>
2017-01-12 16:21:21 +0100

    Added qr code functionality

--- TicketsRes/TicketsReservation/models.py
+++ TicketsRes/TicketsReservation/models.py
@@ -1,7 +1,12 @@
 from __future__ import unicode_literals
-from django.db import models
+
+import StringIO
+import qrcode
 
 from EventCreator.models import Sector, Event
+from django.core.files.uploadedfile import InMemoryUploadedFile
+from django.core.urlresolvers import reverse
+from django.db import models
 
 
 class Tickets(models.Model):
@@ -12,3 +17,32 @@ class Tickets(models.Model):
     guest_name = models.CharField(max_length=32)
     guest_surname = models.CharField(max_length=32)
     guest_email = models.CharField(max_length=32)
+    qrcode = models.ImageField(upload_to='qrcode', blank=True, null=True)
+
+    def get_absolute_url(self):
+        return reverse('view_event', args=[str(self.id)])
+
+    def get_reservation_info(self):
+        event_details = 'Event details:\n' + ' - name: ' + str(self.event.name) + '\n - address: ' + str(self.event.address) + '\n - description: ' + str(self.event.description) + '\n - date: ' + str(self.event.date)
+        sector_details = '\n\nSector: ' + str(self.sector.name)
+        quest_details = '\n\nPersonal info:\n' + ' - name: ' + str(self.guest_name) + ' ' + str(self.guest_surname) + '\n - email: ' + str(self.guest_email)
+        return event_details + sector_details + quest_details
+
+    def generate_qrcode(self):
+        qr = qrcode.QRCode(
+            version=1,
+            error_correction=qrcode.constants.ERROR_CORRECT_L,
+            box_size=6,
+            border=0,
+        )
+        qr.add_data('www.google.com')
+        qr.make(fit=True)
+
+        img = qr.make_image()
+
+        buffer = StringIO.StringIO()
+        img.save(buffer)
+        filename = 'events-%s.png' % (self.id)
+        filebuffer = InMemoryUploadedFile(
+            buffer, None, filename, 'image/png', buffer.len, None)
+        self.qrcode.save(filename, filebuffer)
--- TicketsRes/TicketsReservation/views.py
+++ TicketsRes/TicketsReservation/views.py
@@ -4,6 +4,7 @@ from django.core.mail import send_mail
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.template import loader
+from django.core.mail import EmailMessage
 
 from forms import TicketForm
 from models import Event
@@ -34,10 +35,12 @@ def place_reservation(request, event_id):
             ticket = form.save(commit=False)
             ticket.user = request.user
             ticket.save()
+            ticket.generate_qrcode()
 
-            data = form.cleaned_data
-            email = data['guest_email']
-            send_mail('Test', 'test', 'janusze.pythona@gmail.com', [email])
+            mail = EmailMessage('Event entry ticket', ticket.get_reservation_info(), 'janusze.pythona@gmail.com', [ticket.guest_email])
+            mail.attach_file(ticket.qrcode.url)
+            mail.send()
+            #send_mail('Test', ticket.qrcode, 'janusze.pythona@gmail.com', [email])
             return render(request, 'user_home.html')
     else:
         form = TicketForm(event_id)
