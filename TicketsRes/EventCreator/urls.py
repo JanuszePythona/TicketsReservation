6182a8c4abae76a390ba6a5cbbff2abeb5350e28
Kamill0 <kamillo1493@gmail.com>
2017-01-12 19:20:57 +0100

    Event cancelation functionality added

--- TicketsRes/EventCreator/urls.py
+++ TicketsRes/EventCreator/urls.py
@@ -4,5 +4,7 @@ from . import views
 
 urlpatterns = [
     url(r'^add_event/$', views.add_event, name='add_event'),
-    url(r'^add_sector/(?P<event_id>[0-9]+)$', views.add_sector, name='add_sector')
+    url(r'^add_sector/(?P<event_id>[0-9]+)$', views.add_sector, name='add_sector'),
+    url(r'^confirm_cancel/(?P<event_id>[0-9]+)$', views.confirm_cancel, name='confirm_cancel'),
+    url(r'^cancel_event/(?P<event_id>[0-9]+)$', views.cancel_event, name='cancel_event')
 ]
--- TicketsRes/EventCreator/views.py
+++ TicketsRes/EventCreator/views.py
@@ -2,10 +2,13 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
 from TicketsReservation.views import view_event
-from TicketsReservation.views import view_event
 from forms import EventForm
 from forms import SectorForm
-
+from django.template import loader
+from django.http import HttpResponse
+from models import Event
+from TicketsReservation.models import Tickets
+from django.core.mail import EmailMessage
 
 @login_required
 def add_event(request):
@@ -35,3 +38,25 @@ def add_sector(request, event_id):
     else:
         form = SectorForm(event_id)
     return render(request, 'add_sector.html', {'form': form})
+
+
+def confirm_cancel(request, event_id):
+    template = loader.get_template('confirm_cancelation.html')
+    context = {
+        'event': Event.objects.get(id=event_id)
+    }
+    return HttpResponse(template.render(context, request))
+
+
+def cancel_event(request, event_id):
+    event = Event.objects.get(id=event_id)
+
+    tickets = Tickets.objects.filter(event_id = event_id)
+
+    for ticket in tickets:
+        content = 'We are sorry to inform that the event: ' + str(ticket.event.name) + ' was canceled.'
+        mail = EmailMessage('Event canceled', content, 'janusze.pythona@gmail.com', [ticket.guest_email])
+        mail.send()
+
+    event.delete()
+    return render(request, 'user_home.html')
