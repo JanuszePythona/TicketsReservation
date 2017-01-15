b01073ee53ccbe9ef8232e568969f1466c4bb4d0
Patryk <Matwijczuk>
2017-01-15 13:03:34 +0100

    added validation for ticket

--- TicketsRes/TicketsReservation/forms.py
+++ TicketsRes/TicketsReservation/forms.py
@@ -3,15 +3,42 @@ from django import forms
 
 from models import Tickets
 from models import Sector
+from django.db.models import Q
 
 
 class TicketForm(ModelForm):
+    event_tmp = 0
+
     def __init__(self, event_id, *args, **kwargs):
         super(TicketForm, self).__init__(*args, **kwargs)  # populates the post
-        self.fields['sector'].queryset = Sector.objects.filter(event = event_id)
+        event_tmp = event_id
+        self.fields['sector'].queryset = Sector.objects.filter(event=event_id)
         self.fields['event'].initial = event_id
         self.fields['event'].widget = forms.HiddenInput()
 
     class Meta:
         model = Tickets
         fields = ['event', 'guest_name', 'guest_surname', 'guest_email', 'column', 'row', 'sector']
+
+    def clean(self):
+        row = self.cleaned_data.get('row')
+        column = self.cleaned_data.get('column')
+        sector = self.cleaned_data.get('sector')
+        event = self.cleaned_data.get('event')
+
+        is_free = False
+        try:
+            ticket = Tickets.objects.get(
+                Q(row=self.cleaned_data['row'])
+                & Q(column=self.cleaned_data['column'])
+                & Q(event=self.cleaned_data['event'])
+                & Q(sector=self.cleaned_data['sector'])
+            )
+        except Tickets.DoesNotExist:
+            is_free = True
+
+        if row < 0 | column < 0:
+            raise forms.ValidationError('Incorrect row or column')
+        elif not is_free:
+            raise forms.ValidationError('Ticket was booked by someone else')
+
